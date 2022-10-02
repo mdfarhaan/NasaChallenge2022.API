c86d27b1f924e55c7d355d336cc145fd8972a178
mdfarhaan <farhaanm110@gmail.com>
2022-10-02 21:26:40 +0530

    Fix PORT Config

--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 exports.config = {
   DB_URI: process.env.DB_URI,
-  PORT: 8000,
+  PORT: process.env.PORT || 8000,
   S3AccessKey: process.env.S3AccessKey,
   S3SecretAccessKey: process.env.S3SecretAccessKey,
   S3Bucket: process.env.S3Bucket,
